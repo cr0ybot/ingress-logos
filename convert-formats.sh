2e19ad2d1c104b1f3a8e56a2a39fc5985aa6ce20
Ra100 <ra100x@gmail.com>
2015-06-22 10:44:01 +0200

    new: fan content

--- convert-formats.sh
+++ convert-formats.sh
@@ -3,10 +3,22 @@
 for folder in */ ; do
 	for file in ./$folder*.svg
 	do
-		if [! -f "./$folder`basename $file .svg`.png"]; then
+		if [ ! -f "./$folder`basename $file .svg`.png" ]; then
 			/usr/bin/inkscape -z -f "${file}" -w 640 -e "./$folder`basename $file .svg`.png"
 		fi
-		if [! -f "./$folder`basename $file .svg`.png"]; then
+		if [ ! -f "./$folder`basename $file .svg`.png" ]; then
+			/usr/bin/inkscape -f "${file}" -E "./$folder`basename $file .svg`.eps"
+		fi
+	done
+done
+
+for folder in ./*/*/ ; do
+	for file in ./$folder*.svg
+	do
+		if [ ! -f "./$folder`basename $file .svg`.png" ]; then
+			/usr/bin/inkscape -z -f "${file}" -w 640 -e "./$folder`basename $file .svg`.png"
+		fi
+		if [ ! -f "./$folder`basename $file .svg`.png" ]; then
 			/usr/bin/inkscape -f "${file}" -E "./$folder`basename $file .svg`.eps"
 		fi
 	done
